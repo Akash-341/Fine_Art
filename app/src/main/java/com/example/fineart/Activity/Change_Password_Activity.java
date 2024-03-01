591ffa11c66ab4a8a4502a153e542cf2c1a81225
Dell <telagadesuraj581998@gmail.com>
2024-03-01 18:23:28 +0530

    Commit by suraj at 18.30 PM 1 Mar

---
+++ app/src/main/java/com/example/fineart/Activity/Change_Password_Activity.java
@@ -0,0 +1,16 @@
+package com.example.fineart.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fineart.R;
+
+public class Change_Password_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_change_password);
+    }
+}
---
+++ app/src/main/java/com/example/fineart/Activity/Profile_Activity.java
@@ -0,0 +1,16 @@
+package com.example.fineart.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fineart.R;
+
+public class Profile_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
