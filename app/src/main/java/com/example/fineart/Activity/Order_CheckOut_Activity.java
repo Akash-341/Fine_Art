39744b4d2e46c6b463847612d481db797d041308
Dell <telagadesuraj581998@gmail.com>
2024-03-05 15:11:37 +0530

    Commit by suraj at 15.30 PM 5 Mar

---
+++ app/src/main/java/com/example/fineart/Activity/Order_CheckOut_Activity.java
@@ -0,0 +1,16 @@
+package com.example.fineart.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.fineart.R;
+
+public class Order_CheckOut_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_check_out);
+    }
+}
